e9c8e8c73f2b0084022a10612c6d8f6699ecc755
ismail <coderismail22@gmail.com>
2024-05-01 16:37:54 +0600

    interface

---
+++ src/interface.ts
@@ -0,0 +1,14 @@
+{
+  //Interface
+  interface User {
+    name: string;
+    age: number;
+    address?: string;
+    maritalStatus?: string;
+  }
+
+  const newUser: User = {
+    name: "ismail",
+    age: 19,
+  };
+}
