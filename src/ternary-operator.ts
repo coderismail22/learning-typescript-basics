9e21d32dd83ecdf0c156b51b4ffbbbbe14284b7c
ismail <coderismail22@gmail.com>
2024-05-01 17:59:50 +0600

    ternary-operator

---
+++ src/ternary-operator.ts
@@ -0,0 +1,12 @@
+{
+    // Ternary Operator
+    //Structure: condition?expIfTrue:expIfFalse;
+    
+    let value = ""; // Works on all the falsy values [null, undefined, 0, false,"", NaN]
+
+    const response = value ? "You can go" : "Stop !!!"
+
+    console.log(response)
+
+
+}
