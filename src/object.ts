45e2fc67a805f40cecbc2e564ccb1d7241460ffc
ismail <coderismail22@gmail.com>
2024-05-01 16:37:08 +0600

    objects

---
+++ src/object.ts
@@ -0,0 +1,12 @@
+{
+  const developer: {
+    name: string;
+    age: number;
+    address: string;
+    maritalStatus?: string;
+  } = {
+    name: "ismail",
+    age: 19,
+    address: "BD",
+  };
+}
