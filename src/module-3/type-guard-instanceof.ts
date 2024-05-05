1b67bb5b50d6d6f5fedf6f3a5cf0cb2cdf200bae
ismail <coderismail22@gmail.com>
2024-05-05 17:36:28 +0600

    type-guard-instanceof

---
+++ src/module-3/type-guard-instanceof.ts
@@ -0,0 +1,69 @@
+{
+  //instanceof
+
+  class Animal {
+    name: string;
+    species: string;
+    color: string;
+
+    constructor(name: string, species: string, color: string) {
+      this.name = name;
+      this.species = species;
+      this.color = color;
+    }
+
+    makeSound() {
+      console.log("Making Sound...");
+    }
+  }
+
+  //cat class
+  class Cat extends Animal {
+    constructor(name: string, species: string, color: string) {
+      super(name, species, color);
+    }
+    makeMeow() {
+      console.log("Meow Meow...");
+    }
+  }
+
+  //dog class
+  class Dog extends Animal {
+    constructor(name: string, species: string, color: string) {
+      super(name, species, color);
+    }
+    makeBark() {
+      console.log("Bark Bark...");
+    }
+  }
+
+  //Smart Way of Type Narrowing
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Cat;
+  };
+
+  const playWithAnimals = (animal: Animal) => {
+    if (isDog(animal)) {
+      console.log("it's a dog");
+      animal.makeBark();
+    } else if (isCat(animal)) {
+      console.log("it's a cat");
+      animal.makeMeow();
+    } else {
+      console.log("it's neither dog nor cat");
+      animal.makeSound();
+    }
+  };
+
+  //Use Case:
+  const dog = new Dog("BullDog", "German Shepard", "White");
+  const cat = new Cat("Kitty", "CattyKitty", "Black");
+  const parrot = new Animal("Kitty", "CattyKitty", "Black");
+
+  playWithAnimals(dog); // Bark Bark ...
+  playWithAnimals(cat); // Meow Meow ...
+  playWithAnimals(parrot); // Making Sound (Default) ...
+}
