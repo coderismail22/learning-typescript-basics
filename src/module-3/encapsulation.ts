b8d6a90a10ec1c4ec13e67c74c38df9f83ce43cf
ismail <coderismail22@gmail.com>
2024-05-06 18:58:45 +0600

    encapsulation

---
+++ src/module-3/encapsulation.ts
@@ -0,0 +1,22 @@
+{
+  //encapsulation
+
+  class Car {
+    private _speed: number;
+    constructor(_speed: number) {
+      this._speed = _speed;
+    }
+
+    accelerate(speedIncrease: number): void {
+      this._speed += speedIncrease;
+    }
+
+    get Speed() {
+      return this._speed;
+    }
+  }
+
+  const hondaCar = new Car(0);
+  hondaCar.accelerate(500);
+  console.log(hondaCar.Speed);
+}
